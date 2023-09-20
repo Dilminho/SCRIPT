711c444ec29f13e06ad3939a0985d697d786fdb2
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-20 09:43:47 -0300

    node

---
+++ node/opcao.js
@@ -0,0 +1,12 @@
+function nomeComIdade(nome, idade){
+    if(idade === undefined)[
+        console.log('Seu nome é '+nome)
+    ]
+    else{
+        console.log(`O seu nome é ${nome}, e você tem ${idade}`)
+    }
+}
+
+//nomeComIdade("Epaminondas")
+//nomeComIdade("Carla", 18)
+nomeComIdade(18)
